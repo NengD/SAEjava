2aad8097302bc869cf3cf21d54f1ff38a56abda2
Moreau Quentin <o22400658@or-iut-i207-f19.iut45.univ-orleans.fr>
2025-04-25 11:27:31 +0200

    ajout des fichiers principaux

---
+++ src/Client.java
@@ -0,0 +1,13 @@
+public class Client extends Personne {
+
+    private String adresse;
+
+
+    public Client(String nom, String prenom, String adresse) {
+        super(nom, prenom);
+        this.adresse = adresse;
+    }
+    public void passserCommande() {
+        
+    }
+}
