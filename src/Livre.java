7b6bc7e43ddb9b5d3ec211f93fe889ad72271217
Schmit Clement <o22407286@or-iut-i207-f29.iut45.univ-orleans.fr>
2025-05-09 12:00:46 +0200

    recupération des fichier

---
+++ src/Livre.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Livre {
+    private String titre;
+    private String classification;
+    private Double prix;
+    private List<Auteur> auteurs;
+    private Editeur editeur;
+
+    public Livre(String titre, String classification, Double prix, List<Auteur> auteurs, Editeur editeur) {
+        this.titre = titre;
+        this.classification = classification;
+        this.prix = prix;
+        this.auteurs = new ArrayList<>();
+        this.editeur = editeur;
+        for (Auteur auteur : auteurs) {
+            auteur.ajouterLivre(this);
+        }
+    }
+
+
+}
---
+++ src/Vendeur.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class Vendeur extends Personne {
+
+    private Magasin magasin;
+
+    public Vendeur(String nom, String prenom) {
+        super(nom, prenom);
+    }
+
+    public void ajouterLivre(Livre livre, int quantite){
+        this.magasin..stock.ajouterLivre(livre, quantite);
+    }
+
+    public void majQuantiteLivre(Livre livre, int quantite){
+        this.magasin.stock.ajouterLivre(livre, quantite);
+    }
+
+    public boolean livreDisponible(String nomLivre){
+
+    }
+
+    public void passerCommandePourClient(boolean enLigne,String typeLivraison,List<Livre> livres,Client client){
+
+    }
+}
