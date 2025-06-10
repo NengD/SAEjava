a5d2d287e2417d2afe7b4163ee6de61b778ff9a6
Quentin-Xioz <quentin.moreau@etu.univ-orleans.fr>
2025-06-10 10:09:23 +0200

    Mise à jour de l'App

--- src/App.java
+++ src/App.java
@@ -1,135 +1,174 @@
+
+import java.sql.SQLException;
+
 public class App {
-	private ConnexionMySQL connexionSQL;
+
+    private ConnexionMySQL connexionSQL;
     public boolean quitter;
 
-    public App(){
+    public App() {
         this.quitter = false;
     }
 
     public void lancer() {
-        while(!this.quitter) {
+        while (!this.quitter) {
             menuConnexion();
         }
         quitter();
     }
 
-	public void menuConnexion() {
-	    boolean commande_faite = false;
-	    while(!commande_faite) {
-		    System.out.println("╭────────────────╮");
-		    System.out.println("│ Menu Connexion │");
-		    System.out.println("╰────────────────╯");
-            System.out.println("C: Menu Client");
-	        System.out.println("Q: Quitter");
+    public void menuConnexion() {
+        boolean commande_faite = false;
+        while (!commande_faite) {
+            System.out.println("╭────────────────╮");
+            System.out.println("│ Menu Connexion │");
+            System.out.println("╰────────────────╯");
+            System.out.println("P: Menu principal");
+            System.out.println("Q: Quitter");
 
             /// Majuscule et minuscule prise en compte
 	        String commande_brute = System.console().readLine();
-	        String commande = commande_brute.strip().toLowerCase();
+            String commande = commande_brute.strip().toLowerCase();
 
-	        if(commande.equals("q")) {
+            if (commande.equals("q")) {
                 this.quitter = true;
-                commande_faite = true;}
-            else if(commande.equals("c")){
-				try {
-            		this.connexionSQL = new ConnexionMySQL();
-					menuPrincipal();
-        		}catch (ClassNotFoundException ex){
-            		System.out.println("Driver MySQL non trouvé!!!");
-            		System.exit(1);
-        		}
-			}
+                commande_faite = true;
+            } else if (commande.equals("p")) {
+                try {
+                    this.connexionSQL = new ConnexionMySQL();
+                    boolean connexion_faite = false;
+                    String login = "";
+                    String mdp = "";
+                    while (!connexion_faite) {
+                        if (login.equals("")) {
+                            System.out.println("Quel est votre login");
+                            login = System.console().readLine();
+                        } else if (mdp.equals("")) {
+                            System.out.println("Quel est votre mot de passe");
+                            mdp = System.console().readLine();
+                        } else {
+                            connexion_faite = true;
+                        }
+                    }
+                    this.connexionSQL.connecter(login, mdp);
+                    System.out.println("Connexion réussie à la base de données !");
+                    menuPrincipal();
+                } catch (ClassNotFoundException ex) {
+                    System.out.println("Driver MySQL non trouvé !");
+                    System.exit(1);
+                } catch (SQLException ex) {
+                    System.out.println("Login ou mot de passe incorrect !");
+                    System.exit(1);
+                }
+            }
         }
     }
 
     public void menuPrincipal() {
-	    boolean commande_faite = false;
-	    while(!commande_faite) {
-		    System.out.println("╭────────────────╮");
-		    System.out.println("│ Menu principal │");
-		    System.out.println("╰────────────────╯");
-	        System.out.println("Que voulez vous faire?");
+        boolean commande_faite = false;
+        while (!commande_faite) {
+            System.out.println("╭────────────────╮");
+            System.out.println("│ Menu principal │");
+            System.out.println("╰────────────────╯");
+            System.out.println("Que voulez vous faire?");
             System.out.println("A: Menu Administrateur");
             System.out.println("V: Menu Vendeur");
             System.out.println("C: Menu Client");
-	        System.out.println("Q: Quitter");
+            System.out.println("Q: Quitter");
 
             /// Majuscule et minuscule prise en compte
 	        String commande_brute = System.console().readLine();
-	        String commande = commande_brute.strip().toLowerCase();
+            String commande = commande_brute.strip().toLowerCase();
 
-	        if(commande.equals("q")) {
+            if (commande.equals("q")) {
                 this.quitter = true;
-                commande_faite = true;}
-            else if(commande.equals("a")){
-			    menuAdmin();}
-		    else if(commande.equals("v")){
-			    menuVendeur();}
-		    else if(commande.equals("c")){
-			    menuClient();}
+                commande_faite = true;
+            } else if (commande.equals("a")) {
+                menuAdmin();
+            } else if (commande.equals("v")) {
+                menuVendeur();
+            } else if (commande.equals("c")) {
+                menuClient();
+            }
         }
     }
 
     public void menuAdmin() {
-	    boolean commande_faite = false;
-	    while(!commande_faite) {
-		    System.out.println("╭─────────────────────╮");
-		    System.out.println("│ Menu Administrateur │");
-		    System.out.println("╰─────────────────────╯");
-	        System.out.println("Que voulez vous faire?");
-	        System.out.println("R: Retourner menu principal");
+        boolean commande_faite = false;
+        while (!commande_faite) {
+            System.out.println("╭─────────────────────╮");
+            System.out.println("│ Menu Administrateur │");
+            System.out.println("╰─────────────────────╯");
+            System.out.println("Que voulez vous faire?");
+            System.out.println("R: Retourner menu principal");
+            System.out.println("C: Crée un compte vendeur");
+            System.out.println("A: Ajouter une nouvelle librairie");
+            System.out.println("G: Gérer les stocks de toutes les librairies");
+            System.out.println("S: Consulter les statistiques de vente");
 
             /// Majuscule et minuscule prise en compte
 	        String commande_brute = System.console().readLine();
-	        String commande = commande_brute.strip().toLowerCase();
+            String commande = commande_brute.strip().toLowerCase();
 
-	        if(commande.equals("r")) {
+            if (commande.equals("r")) {
                 this.quitter = true;
-                commande_faite = true;}
+                commande_faite = true;
+            }
         }
     }
-    
+
     public void menuVendeur() {
-	    boolean commande_faite = false;
-	    while(!commande_faite) {
-		    System.out.println("╭──────────────╮");
-		    System.out.println("│ Menu Vendeur │");
-		    System.out.println("╰──────────────╯");
-	        System.out.println("Que voulez vous faire?");
-	        System.out.println("R: Retourner menu principal");
+        boolean commande_faite = false;
+        while (!commande_faite) {
+            System.out.println("╭──────────────╮");
+            System.out.println("│ Menu Vendeur │");
+            System.out.println("╰──────────────╯");
+            System.out.println("Que voulez vous faire?");
+            System.out.println("R: Retourner menu principal");
+            System.out.println("A: Ajouter un livre au stock de sa librairie");
+            System.out.println("M: Mettre à jour la quantité disponible d’un livre");
+            System.out.println("D: Vérifier la disponibilité d’un livre dans une librairie");
+            System.out.println("P: Passer une commande pour un client en magasin");
+            System.out.println("T: Transférer un livre d’une autre librairie pour satisfaire une commande client");
 
             /// Majuscule et minuscule prise en compte
 	        String commande_brute = System.console().readLine();
-	        String commande = commande_brute.strip().toLowerCase();
+            String commande = commande_brute.strip().toLowerCase();
 
-	        if(commande.equals("r")) {
+            if (commande.equals("r")) {
                 this.quitter = true;
-                commande_faite = true;}
+                commande_faite = true;
+            }
         }
     }
 
     public void menuClient() {
-	    boolean commande_faite = false;
-	    while(!commande_faite) {
-		    System.out.println("╭─────────────╮");
-		    System.out.println("│ Menu Client │");
-		    System.out.println("╰─────────────╯");
-	        System.out.println("Que voulez vous faire?");
-	        System.out.println("R: Retourner menu principal");
+        boolean commande_faite = false;
+        while (!commande_faite) {
+            System.out.println("╭─────────────╮");
+            System.out.println("│ Menu Client │");
+            System.out.println("╰─────────────╯");
+            System.out.println("Que voulez vous faire?");
+            System.out.println("R: Retourner menu principal");
+            System.out.println("P: Passer une commande");
+            System.out.println("M: Choisir le mode de réception");
+            System.out.println("C: Consulter le catalogue");
 
             /// Majuscule et minuscule prise en compte
 	        String commande_brute = System.console().readLine();
-	        String commande = commande_brute.strip().toLowerCase();
+            String commande = commande_brute.strip().toLowerCase();
 
-	        if(commande.equals("r")) {
+            if (commande.equals("r")) {
                 this.quitter = true;
-                commande_faite = true;}
+                commande_faite = true;
+            }
         }
     }
 
     /// Affiche un message d'au revoir
     public void quitter() {
-	System.out.println("╭──────────────────────────────────────────────────────╮");
-	System.out.println("│ Au revoir, la librairie reste ouverte et accessible! │");
-	System.out.println("╰──────────────────────────────────────────────────────╯");
+        System.out.println("╭──────────────────────────────────────────────────────╮");
+        System.out.println("│ Au revoir, la librairie reste ouverte et accessible! │");
+        System.out.println("╰──────────────────────────────────────────────────────╯");
     }
 }
--- src/ConnexionMySQL.java
+++ src/ConnexionMySQL.java
@@ -1,28 +1,33 @@
 import java.sql.*;
 
 public class ConnexionMySQL {
-	private Connection mysql=null;
-	private boolean connecte=false;
-	public ConnexionMySQL() throws ClassNotFoundException{
-		Class.forName("org.mariadb.jdbc.Driver");
-	}
 
-	public void connecter(String nomLogin, String motDePasse) throws SQLException {
-		this.mysql=DriverManager.getConnection("jdbc:mysql://servinfo-maria:3306/Librairie",nomLogin, motDePasse);
-		this.connecte=this.mysql!=null;
-	}
-	public void close() throws SQLException {
-		// fermer la connexion
-		this.connecte=false;
-	}
+    private Connection mysql = null;
+    private boolean connecte = false;
 
-    	public boolean isConnecte() { return this.connecte;}
-	public Statement createStatement() throws SQLException {
-		return this.mysql.createStatement();
-	}
+    public ConnexionMySQL() throws ClassNotFoundException {
+        Class.forName("org.mariadb.jdbc.Driver");
+    }
 
-	public PreparedStatement prepareStatement(String requete) throws SQLException{
-		return this.mysql.prepareStatement(requete);
-	}
-	
+    public void connecter(String nomLogin, String motDePasse) throws SQLException {
+        this.mysql = DriverManager.getConnection("jdbc:mysql://servinfo-maria:3306/Librairie", nomLogin, motDePasse);
+        this.connecte = this.mysql != null;
+    }
+
+    public void close() throws SQLException {
+        this.connecte = false;
+    }
+
+    public boolean isConnecte() {
+        return this.connecte;
+    }
+
+    public Statement createStatement() throws SQLException {
+        return this.mysql.createStatement();
+    }
+
+    public PreparedStatement prepareStatement(String requete) throws SQLException {
+        return this.mysql.prepareStatement(requete);
+    }
+
 }
